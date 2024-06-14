9f059002f830ab009afddc41b8b9e9e85cc3da8b
Minh Tri <trinnmse184800@fpt.edu.vn>
2024-06-14 13:56:43 -0700

    react's components part 2

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,56 +1,18 @@
 import React from 'react'
-
+import players from '../shared/ListOfPlayers'
 export default function Players() {
   return (
     <div className='Container'>
-        <div className='Column'>
-            <div className='Card'>
-                <img src='images/cr.jpg' alt=''/>
-                <h2>Christiano Ronaldo</h2>
-                <p className='title'>Manchester Unitied</p>
-                <button className='Detail' href='#Detail'>Detail</button>
+        {players.map((player)=>(
+            <div className='Column'>
+                <div className='Card'>
+                    <img src={player.img} alt=''/>
+                    <h2>{player.name}</h2>
+                    <p className='title'>{player.club}</p>
+                    <button>Detail</button>
+                </div>
             </div>
-        </div>
-        <div className='Column'>
-            <div className='Card'>
-                <img src='images/kante.jpg' alt=''/>
-                <h2>N'Colo Kante</h2>
-                <p className='title'>Chelsea</p>
-                <button className='Detail' href='#Detail'>Detail</button>
-            </div>
-        </div>
-        <div className='Column'>
-            <div className='Card'>
-                <img src='images/messi.jpg' alt=''/>
-                <h2>Lionel messi</h2>
-                <p className='title'>PSG</p>
-                <button className='Detail' href='#Detail'>Detail</button>
-            </div>
-        </div>
-        <div className='Column'>
-            <div className='Card'>
-                <img src='images/neymar.jpg' alt=''/>
-                <h2>Neymar Junior</h2>
-                <p className='title'>PSG</p>
-                <button className='Detail' href='#Detail'>Detail</button>
-            </div>
-        </div>
-        <div className='Column'>
-            <div className='Card'>
-                <img src='images/kane.jpg' alt=''/>
-                <h2>Harry kane</h2>
-                <p className='title'>Tottenham</p>
-                <button className='Detail' href='#Detail'>Detail</button>
-            </div>
-        </div>
-        <div className='Column'>
-            <div className='Card'>
-                <img src='images/haaland.jpg' alt=''/>
-                <h2>Erling Haaland</h2>
-                <p className='title'>Manchester City</p>
-                <button className='Detail' href='#Detail'>Detail</button>
-            </div>
-        </div>
+        ))}
     </div>
   )
 }
---
+++ src/shared/ListOfPlayers.js
@@ -0,0 +1,9 @@
+const players= [
+    {id:1, name:'Christiano Ronaldo', club:'Manchester Unitied', img:'images/cr.jpg'},
+    {id:2, name:'NColo Kante', club: 'Chelsea',img:'images/kante.jpg'},
+    {id:3,name:'Lionel Messi',club:'PSG', img:'images/messi.jpg'},
+    {id:4,name:'Neymar Junior',club: 'PSG', img: 'images/neymar.jpg'},
+    {id: 5,name:'Harry Kane',club:'Tottenham',img: 'images/kane.jpg'},
+    {id:6,name:'Erling Haaland', club:'Manchester City',img: 'images/haaland.jpg'},
+];
+export default players
